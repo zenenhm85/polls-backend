d84d70991b5060a620dc5176729da5d630b8151f
Zenén Hernández Martínez <zenenhernadez85@gmail.com>
2020-09-21 00:02:27 -0400

    search items by params

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -1,6 +1,7 @@
 const { response, request } = require("express");
 
 const User = require("../models/users.model");
+const Poll = require("../models/polls.model");
 
 getUsers = async (req = request, res = response) => {
   const search = req.params.search;
@@ -24,7 +25,30 @@ getUsers = async (req = request, res = response) => {
     });
   }
 };
+getPolls = async (req = request, res = response) => {
+  const search = req.params.search;
+  const limit = Number(req.query.limit) || 5;
+
+  try {
+    const regexp = new RegExp(search, "i");
+
+    const polls = await Poll.find({ question: regexp }).limit(limit);
+
+    return res.status(200).json({
+      ok: true,
+      total: polls.length,
+      polls,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      message: "Server error",
+      errors: error,
+    });
+  }
+};
 
 module.exports = {
   getUsers,
+  getPolls
 };
--- routes/search.route.js
+++ routes/search.route.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 
-const {validateJWT} = require('../middlewares/validate-jwt');
-const {getUsers} = require('../controllers/search.controller');
+const { validateJWT } = require("../middlewares/validate-jwt");
+const {getUsers, getPolls} = require('../controllers/search.controller');
 
 const router = Router();
 
 
 router.get('/users/:search',validateJWT, getUsers);
+router.get('/polls/:search',validateJWT, getPolls);
 
 
 module.exports = router;
